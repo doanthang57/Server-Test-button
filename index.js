028568d44a85082aec859f0527adad9d42e64435
doanthang <doanthang57@gmail.com>
2018-05-14 18:06:40 +0700

    initial commit

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 var awsIot = require('aws-iot-device-sdk');
-
 var SerialPort = require("serialport");
-var serialPort = new SerialPort('/dev/tty-usbserial1', {
-  baudRate: 57600);
+var serialPort = new SerialPort("COM1", { baudRate: 115200 });
 
-var status='0';
-var data;
 
-const express = require("express");
-const app = express();
-const  server = require("http").Server(app);
-const io = require("socket.io")(server);
-
-app.use(express.static("./public"));  //noi dat cac file css, script.js
-app.set("view engine","ejs");   //File html cua minh se la ejs
-app.set('views',__dirname +'/views');     //dat vao tong thu muc views
+var dataden="abc";
+var clientTokenUpdate;
+var sessionid;
+var local_sessionid = '0';//tam thoi
 
 var thingShadows = awsIot.thingShadow({
    keyPath: './certs/ef21fe66d7-private.pem.key',
@@ -23,167 +15,236 @@ var thingShadows = awsIot.thingShadow({
   clientId: 'nameThang',
       host: 'aupu5tnzgckec.iot.ap-southeast-1.amazonaws.com'
 });
-var clientTokenUpdate;
-//
-// Simulated device values
-//
-var den1="on";
-var den2="off";
-// thingShadows.on('connect', function() {
-// //
-// // After connecting to the AWS IoT platform, register interest in the
-// // Thing Shadow named 'RGBLedLamp'.
-// //
-//     thingShadows.register( 'Thang-Test', {}, function() {
-// // Once registration is complete, update the Thing Shadow named
-// // 'RGBLedLamp' with the latest device state and save the clientToken
-// // so that we can correlate it with status or timeout events.
-//
-// // Thing shadow state
-// //
-//        var rgbLedLampState = {
-//          "state":{
-//            "desired":{"den1": den1,"den2": den2}
-//          }
-//        };
-//
-//        clientTokenUpdate = thingShadows.update('Thang-Test', rgbLedLampState  );
-//        // The update method returns a clientToken; if non-null, this value will
-//        // be sent in a 'status' event when the operation completes, allowing you
-//        // to know whether or not the update was successful.  If the update method
-//        // returns null, it's because another operation is currently in progress and
-//        // you'll need to wait until it completes (or times out) before updating the
-//        // shadow.
-//        //
-//               if (clientTokenUpdate === null)
-//               {
-//                  console.log('update shadow failed, operation still in progress');
-//               }
-//            });
-//        });
-//
-//        thingShadows.on('delta',
-//            function(thingName, stateObject) {
-//               console.log(stateObject.state.den1);
-//               serialPort.write(stateObject.state.den1); // Gui gia tri tu aws shadow ve cong serial
-//
-//            });
-//
-//        thingShadows.on('timeout',
-//            function(thingName, clientToken) {
-//               console.log('received timeout on '+thingName+
-//                           ' with token: '+ clientToken);
-//        //
-//        // In the event that a shadow operation times out, you'll receive
-//        // one of these events.  The clientToken value associated with the
-//        // event will have the same value which was returned in an earlier
-//        // call to get(), update(), or delete().
-//        //
-//  });
+var i = 0;
+var buffer = {
+  "container": {
+    "action": "turn on",
+    "device": "light",
+    "id": "01",
+    "room": "living room",
+    "mode": "set",
+    "sessionid": "1",
+    "lux": "100"
+  }
+}
+var rgbLedLampState = {
+  "state":{
+    "desired":{"Data": buffer}
+  }
+}
 
-server.listen(3000,function(){
-  console.log("server is running on port 3000");
-});  //cong port 3000
+// lang nghe thingShadow
+thingShadows.on('connect', function() {
+thingShadows.register( 'Thang-Test', {}, function() {
+    var rgbLedLampState = {
+      "state":{
+        "desired":{"Wellcome": dataden}
+      }
+    }
+// gui len ssid local++
+// Update data len IOT
+    clientTokenUpdate = thingShadows.update('Thang-Test', rgbLedLampState  );
+    console.log('update shadow');
 
- //*********su dung module serialport*******?//
-
-app.get('/',(req, res) => {
-   res.render("home");
+              if (clientTokenUpdate === null)
+              {
+                 console.log('update shadow failed, operation still in progress');
+             }
+  });
 });
 
+thingShadows.on('delta',
+    function(thingName, stateObject) {
+       console.log('SSID: '+ JSON.stringify(stateObject.state.Data.container.sessionid)); // dang string
+       console.log('SSID2: '+ stateObject.state.Data.container.sessionid); // dang json
+       sessionid = JSON.stringify(stateObject.state.Data.container.sessionid);
+
+       if( sessionid === null)
+       {
+         console.log("Error: SSID rong~");
+       }
+       else {
+          if(sessionid != local_sessionid ) //Neu SSID Thay doi
+          {
+            // convent
+            serialPort.write('@')
+
+            console.log(buffer.container.device);
+            console.log(buffer.container.id);
+
+            if (stateObject.state.Data.container.device == 'Light') {
+              serialPort.write('L')
+              serialPort.write(stateObject.state.Data.container.id)
+            }
+            if (stateObject.state.Data.container.device == 'Fan') {
+              serialPort.write('F')
+              serialPort.write(stateObject.state.Data.container.id)
+            }
+            if (stateObject.state.Data.container.device == 'TV') {
+              serialPort.write('V')
+              serialPort.write(stateObject.state.Data.container.id)
+            }
+
+            console.log(stateObject.state.Data.container.room);
+            if (stateObject.state.Data.container.room == 'Bed Room') {
+              serialPort.write('B')
+            }
+            if (stateObject.state.Data.container.room == 'Kitchen') {
+              serialPort.write('K')
+            }
+            if (stateObject.state.Data.container.room == 'Living Room') {
+              serialPort.write('R')
+            }
+
+            console.log(buffer.container.action);
+            if (stateObject.state.Data.container.action == 'Turn On') {
+              serialPort.write('X')
+            }
+            if (stateObject.state.Data.container.action == 'Turn Off') {
+              serialPort.write('Y')
+            }
+
+            console.log(buffer.container.mode);
+            if (stateObject.state.Data.container.mode == 'Auto') {
+              serialPort.write('A')
+            }
+            if (stateObject.state.Data.container.mode == 'SET') {
+              serialPort.write('S')
+            }
+              serialPort.write('x')
+
+            // updat firmware
+            console.log('OK');
+          }
+          else {
+            console.log('Error 2: SSID trung`');
+          }
+       }
+
+       // console.log('received delta on '+thingName+': '+
+       //             JSON.stringify(stateObject));
+    });
+// thingShadows.on('status',
+//     function(thingName, stat, clientToken, stateObject) {
+//        console.log('\n'+'received '+stat+' on '+thingName+': '+
+//                    JSON.stringify(stateObject));
+// });
+//
+// thingShadows.on('timeout',
+//     function(thingName, clientToken) {
+//        console.log('\n'+'received timeout on '+thingName+
+//                    ' with token: '+ clientToken);
+// });
+
+// Nhan serial PORT tu device
 serialPort.on('open',onOpen);
-console.log('open',onOpen);
+serialPort.on('data',onData);
 
 function onOpen(){
   console.log("Open connected");
 }
+function onData(data){
+  console.log('Data receive: '+data); // show buff nhan duoc
+  console.log('Ondata: '+ String.fromCharCode(data));
 
+  //-----------------@L1OLS100----------------------------
+var n = 8;
+for (var i = 0; i < n; i++) {
+  switch (String.fromCharCode(data[i])) {
+    case '@':
+      if(i == 0)
+      break;
 
-io.on('connection', function (socket) {   //khi cac client ket noi
+    case 'F':
 
-  serialPort.on('data', function(onData){
-    console.log("data: " + onData);
-    if(onData=='@O1D1L29%'){ //so sanh chuoi
-      socket.emit('sv2cl',{value:'1'});
-    }
-    else {
-      socket.emit('sv2cl',{value:'0'});
-    }
-  })
+      if (i == 1) {
+        i++;
+        buffer.container.device = 'Fan';
+        buffer.container.id=String.fromCharCode(data[i]);
+        break;
+      }
 
-        console.log("client connected");
-        socket.emit('sv2cl',{value: status}); //mac dinh khi bat server len thi tat ca cac client co trang thai bang 0
-      //  serialPort.write(status); //nt
+    case 'L':
 
-        socket.on('cl2sv',function(msg)
-        {
+      if (i == 1) {
+        i++;
+        buffer.container.device = 'Light';
+        buffer.container.id=String.fromCharCode(data[i]);
+        break;
+      }
+    case 'V':
 
-      //  serialPort.on('open',onOpen);
-      //  serialPort.on('data',onData);
-        console.log("On data"+ data);
-          if(msg.status[0] == '1'){
-            console.log("Status 1: " + msg.status);
-            serialPort.write(msg.status); //gui du lieu cho serrial port
-            //sockets.emit('led', {value: msg.status });  //phat cho tat cac cac client
+      if (i == 1) {
+        i++;
+        buffer.container.device = 'TV';
+        buffer.container.id=String.fromCharCode(data[i]);
+        break;
+      }
+
+    case 'B':
+      if(i ==3){
+          buffer.container.room = 'Bed Room';
+          break;
+      }
+    case 'K':
+      if(i ==3){
+          buffer.container.room = 'Kitchen Room';
+          break;
+      }
+    case 'R':
+      if(i ==3){
+          buffer.container.room = 'Living Room';
+          break;
           }
-          else {
-            console.log("Status 0: "+msg.status);
-            serialPort.write(msg.status);
-            //socket.emit('led',{value: msg.status });
-          }
-          //serialPort.write(msg.status);
-          socket.emit('led',{value: msg.status});
-      });
 
-});
-thingShadows.on('connect', function() {
-//
-// After connecting to the AWS IoT platform, register interest in the
-// Thing Shadow named 'RGBLedLamp'.
-  var rgbLedLampState = {
-    "state":{
-      "desired":{"den1": den1,"den2": den2}
-    }
+    case 'X':
+      if(i ==4){
+          buffer.container.action = 'Turn On'; // 1 la bat
+          break;
+      }
+    case 'Y':
+      if(i ==4){
+          buffer.container.action = 'Turn Off'; // 0 la tat
+          break;
+      }
+    case 'S':
+      if(i ==5){
+          buffer.container.mode = 'SET';
+          for( i = 6; i< 9; i++)
+          {
+            buffer.container.lux=String.fromCharCode(data[6])+String.fromCharCode(data[7])+String.fromCharCode(data[8]);
+
+            thingShadows.register( 'Thang-Test', {}, function() {
+              clientTokenUpdate = thingShadows.update('Thang-Test', rgbLedLampState  );
+              console.log('Update Susses');
+            })
+          }
+          break;
+      }
+    case 'A':
+      if(i ==5){
+          buffer.container.mode = 'Auto';
+          for( i = 6; i< 9; i++)
+          {
+            buffer.container.lux=String.fromCharCode(data[6])+String.fromCharCode(data[7])+String.fromCharCode(data[8]);
+            thingShadows.register( 'Thang-Test', {}, function() {
+              clientTokenUpdate = thingShadows.update('Thang-Test', rgbLedLampState  );
+              console.log('Update Susses');
+            })
+          }
+
+          break;
+      }
+
+    default:
+      i = n - 1;
+      console.log('SAI HET ROI, NHAP LAI CHUOI');
+      thingShadows.register( 'Thang-Test', {}, function() {
+        clientTokenUpdate = thingShadows.update('Thang-Test', rgbLedLampState  );
+        console.log('Update Susses');
+      })
+      break;
   }
-    module.exports = rgbLedLampState;
-    thingShadows.register( 'Thang-Test', {}, function() {
-// Once registration is complete, update the Thing Shadow named
-// 'RGBLedLamp' with the latest device state and save the clientToken
-// so that we can correlate it with status or timeout events.
-// Thing shadow state
-//
-
-
-//var lame = require('rgbLedLampState');
-       clientTokenUpdate = thingShadows.update('Thang-Test', rgbLedLampState  );
-       // The update method returns a clientToken; if non-null, this value will
-       // be sent in a 'status' event when the operation completes, allowing you
-       // to know whether or not the update was successful.  If the update method
-       // returns null, it's because another operation is currently in progress and
-       // you'll need to wait until it completes (or times out) before updating the
-       // shadow.
-       //
-              if (clientTokenUpdate === null)
-              {
-                 console.log('update shadow failed, operation still in progress');
-              }
-           });
-       });
-
-       thingShadows.on('delta',
-           function(thingName, stateObject) {
-              console.log(stateObject.state.den1);
-              //serialPort.write(stateObject.state.den1); // Gui gia tri tu aws shadow ve cong serial
-
-           });
-
-       thingShadows.on('timeout',function(thingName, clientToken) {
-              console.log('received timeout on '+thingName+
-                          ' with token: '+ clientToken);
-       //
-       // In the event that a shadow operation times out, you'll receive
-       // one of these events.  The clientToken value associated with the
-       // event will have the same value which was returned in an earlier
-       // call to get(), update(), or delete().
-       //
- });
+}
+console.log(buffer);
+}
